8162db595cfca7b048dd9cc21ec3b4e75ad749ac
Sudhanshu Rathore <rathore.sudhanshu60@gmail.com>
2020-09-30 20:37:22 +0530

    Update : Writer uses queue for transliterating.

--- src/Writer.js
+++ src/Writer.js
@@ -22,6 +22,11 @@ class Writer {
          * which needs to be transliterated.
          */
         this.transliterationQueue = [];
+
+        /**
+         * Being transliteration.
+         */
+        setTimeout(this._transliterate.bind(this), 1000);
     }
 
     /**
@@ -59,9 +64,29 @@ class Writer {
     }
 
     async _transliterate() {
-        const word = this.caret.lastWord();
-        const transliteration = await HindiTransliterator.transliterate(word.text);
-        this.caret.replace(transliteration, word);
+
+        const isTransliterationQueueEmpty = this.transliterationQueue.length === 0;
+
+        if(!isTransliterationQueueEmpty){
+            const word = this.transliterationQueue.shift();
+            const transliteration = await HindiTransliterator.transliterate(word.text);
+            this.caret.replace(transliteration, word);
+        }
+
+        /**
+         * We are going to increase wait
+         * time for next call to 1000 microseconds
+         * if `transliteration queue` is empty.
+         * 
+         */
+        let waitTime = 0;
+        if(isTransliterationQueueEmpty){
+            waitTime = 1000;
+        }
+        /**
+         * We are not going to wait here 
+         */
+        setTimeout(this._transliterate.bind(this), waitTime);
     }
 }
 
