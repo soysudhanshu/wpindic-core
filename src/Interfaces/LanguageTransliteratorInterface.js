218da6ea13e5d7913df83d198703bffa6b690c62
Sudhanshu Rathore <soysudhanshu@gmail.com>
2020-10-30 21:13:10 +0530

    Updated : LanguageTransliteratorInterface errors

--- src/Interfaces/LanguageTransliteratorInterface.js
+++ src/Interfaces/LanguageTransliteratorInterface.js
@@ -2,7 +2,11 @@ import TransliteratorInterface from "./TransliterateInterface";
 
 class LanguageTransliteratorInterface extends TransliteratorInterface{
     static getLang(){
-        
+        throw new Error(`getLang() method must be implemented.`);
+    }
+
+    static _getTransliteration(word){
+        throw new Error(`_getTransliteration() method must be implemented.`);
     }
 }
 
