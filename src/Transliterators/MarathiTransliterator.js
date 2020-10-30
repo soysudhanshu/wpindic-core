c13a12697b9322c780c6a93fbf2f4f3e98c1b60d
Sudhanshu Rathore <soysudhanshu@gmail.com>
2020-10-30 21:18:19 +0530

    Added : New transliterators
    Now available languages are Bangla, Gujrati, Kannada, Malyalam, Marathi, Nepali, Oriya, Punjabi, Sanskrit, Sinhala, Tamil, Telgu and Urdu

---
+++ src/Transliterators/MarathiTransliterator.js
@@ -0,0 +1,53 @@
+import GoogleTransliterator from '../GoogleTransliterator.js';
+import LanguageTransliteratorInterface from '../Interfaces/LanguageTransliteratorInterface.js';
+
+const cache = {};
+
+class MarathiTransliterator  extends LanguageTransliteratorInterface {
+    static transliterate(word) {
+        return new Promise((resolve, reject) => {
+            this._getTransliteration(word)
+                .then(this._addToCache)
+                .then(transliteration => resolve(transliteration.transliterations[0]))
+                .catch(error => reject(error));
+        });
+    }
+
+    static suggest(word) {
+        return new Promise((resolve, reject) => {
+            this._getTransliteration(word)
+                .then(this._addToCache)
+                .then(transliteration => resolve(transliteration.transliterations))
+                .catch(error => reject(error));
+        });
+    }
+
+    static suggestAlternative(transliteratedText) {
+        const possibles = {};
+        for (const key in cache) {
+            const transliteration = cache[key];
+            const possibleTransliterations = transliteration.transliterations;
+            if (possibleTransliterations.includes(transliteratedText)) {
+                for (const text of possibleTransliterations) {
+                    possibles[text] = '';
+                }
+            }
+        }
+        return Object.keys(possibles);
+    }
+
+    static _addToCache(transliteration) {
+        cache[transliteration.original] = transliteration;
+        return transliteration;
+    }
+
+    static _getTransliteration(word){
+        return GoogleTransliterator.transliterate(word, 'mr');
+    }
+
+    static getLang(){
+        return 'mr';
+    }
+}
+
+export default MarathiTransliterator;
